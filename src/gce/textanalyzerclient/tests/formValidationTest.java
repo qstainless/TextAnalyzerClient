e189ff17f47ea65e74924aa39589ee47a028fae9
Queue Stainless <gce517+git@gmail.com>
2020-04-08 21:33:37 -0400

    Added formValidationTest

---
+++ src/gce/textanalyzerclient/tests/formValidationTest.java
@@ -0,0 +1,47 @@
+package gce.textanalyzerclient.tests;
+
+import gce.textanalyzerclient.model.formValidation;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class formValidationTest {
+    static final String nullUrl = null;
+    static final String notEmptyUrl = "http://somewhere.com/file_to_read.html";
+    static final String malformedUrl = "ervsdfavadfa";
+
+    /**
+     * Checks if the URL text field is empty
+     */
+    @Test
+    @Order(1)
+    @DisplayName("The URL field is empty.")
+    void testEmptyUrl() {
+        boolean actualResult = formValidation.textFieldNotEmpty(nullUrl);
+        assertFalse(actualResult);
+    }
+
+    /**
+     * Checks if the URL text field is not empty.
+     */
+    @Test
+    @Order(2)
+    @DisplayName("The URL field is not empty.")
+    void testNotEmptyUrl() {
+        boolean actualResult = formValidation.textFieldNotEmpty(notEmptyUrl);
+        assertTrue(actualResult);
+    }
+
+    /**
+     * Checks if the URL text field is malformed.
+     */
+    @Test
+    @Order(3)
+    @DisplayName("The URL provided is malformed.")
+    void testMalformedUrl() {
+        boolean actualResult = formValidation.textFieldNotEmpty(malformedUrl);
+        assertFalse(actualResult);
+    }
+}

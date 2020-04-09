8efb117dab40b55fa484b9c14858a0dc0fb6b82c
Queue Stainless <gce517+git@gmail.com>
2020-04-08 21:14:56 -0400

    Refactored variable names. Form validation now checks for valid URL.

--- src/gce/textanalyzerclient/model/formValidation.java
+++ src/gce/textanalyzerclient/model/formValidation.java
@@ -2,6 +2,8 @@ package gce.textanalyzerclient.model;
 
 import javafx.scene.control.Label;
 
+import java.net.URL;
+
 /**
  * Validation class to check whether or not the URL text field is empty when
  * the user clicks the Analyze! button. The class does not check for valid
@@ -11,17 +13,16 @@ public class formValidation {
 
     /**
      * {@code out} defaults to false if the input parameter is <tt>null</tt>
-     * or empty.
+     * or empty and if it is a valid URL.
      *
-     * @param input The value of the URL textfield.
-     * @return True if the URL textfield is not empty
-     *
+     * @param targetUrl The URL entered by the user in the URL textfield.
+     * @return True if the URL textfield is not empty and is a valid URL
      * @see formValidation#textFieldNotEmpty(String, Label, String)
      */
-    public static boolean textFieldNotEmpty (String input) {
+    public static boolean textFieldNotEmpty(String targetUrl) {
         boolean out = false;
 
-        if (input != null && !input.isEmpty()) {
+        if (targetUrl != null && !targetUrl.isEmpty() && isValidURL(targetUrl)) {
             out = true;
         }
 
@@ -29,16 +30,16 @@ public class formValidation {
     }
 
     /**
-     * @param input The value of the URL textfield.
-     * @param messageLabel Placeholder in the GUI for error messages
+     * @param targeturl      The value of the URL textfield.
+     * @param messageLabel   Placeholder in the GUI for error messages
      * @param validationText Feedback to user on errors.
      * @return True if the URL textfield is not empty
      */
-    public static boolean textFieldNotEmpty (String input, Label messageLabel, String validationText) {
+    public static boolean textFieldNotEmpty(String targeturl, Label messageLabel, String validationText) {
         boolean out = true;
         String message = null;
 
-        if (!textFieldNotEmpty(input)) {
+        if (!textFieldNotEmpty(targeturl)) {
             out = false;
             message = validationText;
         }
@@ -46,5 +47,21 @@ public class formValidation {
         messageLabel.setText(message);
 
         return out;
+    }
+
+    /**
+     * Checks if the targetUrl is a valid URL
+     *
+     * @param targetUrl The target URL entered by the user
+     * @return True if the URL is valir
+     */
+    public static boolean isValidURL(String targetUrl) {
+        try {
+            URL url = new URL(targetUrl);
+            url.toURI();
+            return true;
+        } catch (Exception exception) {
+            return false;
+        }
     }
 }
